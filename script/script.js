291c6104f1f7d0480ea7b595eea8dd80f5bea999
francescapetra <francescalaurapetra@gmail.com>
2021-03-24 14:36:55 +0100

    istanzio Vue

--- script/script.js
+++ script/script.js
@@ -1,1 +1,12 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
+// istanzio il mio Vue
+var app = new Vue(
+  {
+  element: "#root",
+  }
+);
+console.log(app);
